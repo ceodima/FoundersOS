e58016813f56304f5cbec2591d1c4dd5c41c6d50
ceodima <ceodima@icloud.com>
2025-11-29 18:08:45 +0300

    1.0.3

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ try {
   SDKProvider = ({ children }: { children: React.ReactNode }) => <>{children}</>; // заглушка для dev
 }
 
-import App from './App';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
